fb4c22041b9adf00ef083b39db462fdc1c2fe17d
vsko <info@zoom.net.ua>
2018-03-21 20:34:40 +0200

    Created Sumish minimal application template

---
+++ config/web.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'routes' => [
+        '/' => 'home',
+    ],
+//    'db' => [
+//        'driver' => 'mysql',
+//        'host' => 'zoom.net.ua',
+//        'database' => 'projects',
+//        'user' => 'projects',
+//        'password' => 'projects',
+//        'charset' => 'utf8',
+//    ],
+    'compression' => 6,
+    'mode' => 'web'
+];
